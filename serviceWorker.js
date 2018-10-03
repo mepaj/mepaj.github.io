a57dd5e701475e69bb3604d08f3053f3d9da8618
MEPAJ <39146221+mepaj@users.noreply.github.com>
2018-10-04 05:35:37 +0700

    update SW

--- serviceWorker.js
+++ serviceWorker.js
@@ -11,31 +11,44 @@ self.addEventListener('install', async e => {
     cache.addAll(setAssets);
 });
 
-self.addEventListener('fetch', e => {
-    const req = e.request;
-    const url = new URL(req.url);
-
-    console.log('url.origin === location.origin >>>>>> ', url.origin, location.origin);
-    if (url.origin === location.origin) {
-        e.respondWith(cacheFirst(req));
-    } else {
-        e.respondWith(networkFirst(req));
-    }
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open('news-dynamic').then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
 
-async function cacheFirst(req) {
-    const cacheResponse = await caches.match(req);
-    return cacheResponse || fetch(req);
-}
+// self.addEventListener('fetch', e => {
+//     const req = e.request;
+//     const url = new URL(req.url);
 
-async function networkFirst(req) {
-    const cache = await caches.open('news-dynamic');
+//     // console.log('url.origin === location.origin >>>>>> ', url.origin, location.origin);
+//     if (url.origin === location.origin) {
+//         e.respondWith(cacheFirst(req));
+//     } else {
+//         e.respondWith(networkFirst(req));
+//     }
+// });
 
-    try {
-        const res = await fetch(req);
-        cache.put(req, res.clone());
-        return res;
-    } catch (error) {
-        return await cache.match(req);
-    }
-}
+// async function cacheFirst(req) {
+//     const cacheResponse = await caches.match(req);
+//     return cacheResponse || fetch(req);
+// }
+
+// async function networkFirst(req) {
+//     const cache = await caches.open('news-dynamic');
+
+//     try {
+//         const res = await fetch(req);
+//         cache.put(req, res.clone());
+//         return res;
+//     } catch (error) {
+//         return await cache.match(req);
+//     }
+// }
